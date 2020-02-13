340b77cc7ada22af7cc57cd4f799398aa337bd79
Tomislav Jakopec <tjakopec@gmail.com>
2020-02-13 10:04:53 +0100

    Kreiranje projekata

---
+++ Java/src/Start.java
@@ -0,0 +1,8 @@
+public class Start {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
---
+++ PHP/Start.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace jakopec;
+
+
+class Start
+{
+    public function __construct()
+    {
+        echo "Hello";
+    }
+
+}
+
+new Start();
