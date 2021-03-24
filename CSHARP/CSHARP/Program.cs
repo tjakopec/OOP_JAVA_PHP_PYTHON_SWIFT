ec575e7843b1096f03ba80b0b343fd8be3a1ca82
Tomislav Jakopec <tjakopec@gmail.com>
2021-03-24 14:54:14 +0100

    Kreiranje projekta u C#

---
+++ CSHARP/CSHARP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
